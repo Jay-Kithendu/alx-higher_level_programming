f3b2e833d61cfbb8cb473b06f34a39e4d02f1144
Jay-Kithendu <james98katiwa@gmail.com>
2022-11-07 12:04:54 +0000

    program that prints the ASCII alphabet, in lowercase except q and e, not followed by a new line

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!usr/bin/python3
 for alphabets in range(ord('a'), ord('z')):
-    if alphabet == 'q' and alphabet == 'e':
-        continue
-    print("{:c}".format(alphabets), end="")
+    if (alphabet != 'q' and alphabet != 'e'):
+	print("{:c}".format(alphabets), end="")
